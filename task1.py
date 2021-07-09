02c799a6417757ce6051f14916212c38ced72cbc
Yaroslav <86650368+YaroslavMelnyk08@users.noreply.github.com>
2021-07-09 09:57:33 +0300

    Update task1.py

---
+++ task1.py
@@ -0,0 +1,14 @@
+import re
+string = input("\nВведіть масив: ")
+let = ''.join([i for i in string if not i.isdigit()]) 
+a = re.findall(r'\d+', string) 
+a = [int(i) for i in a] 
+print("\nБукви:", let)
+print("Числа:", a)
+Let = ' '.join(word[0].upper() + word[1:-1] + word[-1:].upper() for word in let.split()) 
+print("\nРядок з велики буквами:", Let)
+print("Максимальний елемент масиву:", max(a)) 
+a.remove(max(a)) 
+numindex = [a[i]**i for i in range(0,len(a))] 
+print("Масив чисел, піднесені до степеню по їх індексу:", numindex)
+print("\n")
